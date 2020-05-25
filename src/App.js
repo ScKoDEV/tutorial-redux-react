56f73cb39e084117746d83187267a1b7c2580bdc
Konstantin <konstantinschiller@gmx.de>
2020-05-25 15:43:13 -0400

    Initial Commit Build React app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Posts from "./components/Posts";
+import PostForm from "./components/PostForm";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to React</h1>
+      </header>
+      <PostForm />
+      <hr />
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
